c3a351ab0f6f726270bd3b1520114305b84e73d9
wpeterr <mycodeisbad@gmail.com>
2023-07-07 12:01:12 -0600

    first commit

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import streamlit as st
 import os
 import openai
+from dotenv import load_dotenv
 
 st.title("Twitter Comment Reply App")
+load_dotenv()
+openai.api_key = os.environ.get('OPENAI_API_KEY')
 
 def generate_response(comment):
-    tweet = "Been doing 15 hr days for the past 3 weeks. Happy to just take a day off and enjoy Spring in NYC :)"
-    openai.api_key = os.getenv("OPENAI_API_KEY")
     response = openai.Completion.create(
-        model="davinci:ft-ddd:codetweetweave3-2023-06-12-20-32-07",
+        model="davinci:ft-offbrand-2023-06-24-04-01-32",
         prompt=f"You are a popular and fun twitter influencer in the AI tech space. Given a comment on your post, reply to other user's tweets. Keep your reply short,one or two sentences at most.Reply should be one sentence long. Comment: " + comment + "\nReply:",
         temperature=1,
         max_tokens=256,
